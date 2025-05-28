15266c5d22cf9f02cda25e91b7e3078a730a75fa
nevermore228 <makarevich.mks@gmail.com>
2025-05-28 17:52:45 +0400

    models.py changes

--- models.py
+++ models.py
@@ -1,28 +1,32 @@
+from sqlalchemy import ForeignKey, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 from datetime import datetime
+from typing import Annotated
 
+
+intpk = Annotated[int, mapped_column(primary_key=True)]                     #   int с функцией mapped_column
 
 class User(Base):
     __tablename__= "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column() 
-    email: Mapped[str] = mapped_column() 
-    hash_pass: Mapped[str] = mapped_column() 
+    id: Mapped[intpk]
+    username: Mapped[str] 
+    email: Mapped[str] 
+    hash_pass: Mapped[str]
 
-    posts = relationship("Post", back_populates="author")
-    comments = relationship("Comment", back_populates="author")
+    post_id: Mapped[str] = mapped_column(ForeignKey("posts.id"))
+    comment_id: Mapped[str] = mapped_column(ForeignKey("comments.id"))
 
 class Post(Base):
-    __tablename__="posts"
+    __tablename__ = "posts"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column() 
-    content: Mapped[str] = mapped_column() 
-    author_id = Column(Integer, ForeignKey('users.id'))
-    date = Column(DateTime, default=datetime.now)
-    image_path: Mapped[str] = mapped_column() 
+    id: Mapped[intpk]
+    title: Mapped[str] = mapped_column(String(50))
+    content: Mapped[str] 
+    author_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
+    date: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
+    image_path: Mapped[str | None] 
     
     author = relationship("User", back_populates="posts")
     comments = relationship("Comment", back_populates="posts")
@@ -31,11 +35,12 @@ class Post(Base):
 class Comment(Base):
     __tablename__="comments"
 
-    id: Mapped[int] = mapped_column(primary_key=True) 
-    content: Mapped[str] = mapped_column() 
+    id: Mapped[intpk]
+    content: Mapped[str] 
     author_id = Column(Integer, ForeignKey('users.id'))
     date = Column(DateTime, default=datetime.now)
     post_id = Column(Integer, ForeignKey('posts.id'))
+    image_path: Mapped[str | None]
 
     author = relationship("User", back_populates="comments")
     posts = relationship("Post", back_populates="comments")
